29e6cbb013dbb02f57ba20c671b84ba50ba825fd
Tero Hasu <tero@hasu.is>
2015-12-20 18:46:44 +0200

    switch to using custom manual-code module

--- main.rkt
+++ main.rkt
@@ -1,6 +1,6 @@
 #lang racket/base
 
-(require scribble/core scribble/manual
+(require scribble/core "manual-code.rkt"
          (for-syntax racket/base
                      syntax/parse))
 
@@ -9,15 +9,23 @@
 ;; from Ben Lerner
 (define-syntax (Cxx-block stx)
   (syntax-parse stx
-    [(_ (~or (~optional (~seq #:indent indent) #:defaults ([indent #'1]))
-             (~optional (~seq #:line-numbers line-numbers) #:defaults ([line-numbers #'#f]))
-             (~optional (~seq #:line-number-sep line-number-sep) #:defaults ([line-number-sep #'1])))
-        ...
-        arg args ...)
-     #'(codeblock #:indent indent #:line-numbers line-numbers #:line-number-sep line-number-sep
-                  #:keep-lang-line? #f #:context #'arg
-                  "#lang cxx-lexer\n" arg args ...)]))
+    [(_ (~seq (~or (~optional
+                    (~seq #:indent indent)
+                    #:defaults ([indent #'0]))
+                   (~optional
+                    (~seq #:line-numbers line-numbers)
+                    #:defaults ([line-numbers #'#f]))
+                   (~optional
+                    (~seq #:line-number-sep line-number-sep)
+                    #:defaults ([line-number-sep #'1])))
+              ...)
+        args ...)
+     #'(codeblock #:indent indent
+                  #:line-numbers line-numbers
+                  #:line-number-sep line-number-sep
+                  #:keep-lang-line? #f
+                  "#lang cxx-lexer\n" args ...)]))
 
-(define-syntax-rule (Cxx arg args ...)
+(define-syntax-rule (Cxx args ...)
   (make-element (make-style "RktBlk" '(tt-chars))
-                (code #:lang "cxx-lexer" arg args ...)))
+                (code #:lang "cxx-lexer" args ...)))
