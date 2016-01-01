864b11f5a30997503a5595d228779f509f41bc96
Tero Hasu <tero@hasu.is>
2016-01-01 16:53:26 +0200

    remove separate license file

--- manual-code.rkt
+++ manual-code.rkt
@@ -180,12 +180,13 @@ it is used under the following license.
 
 Copyright (c) 2010-2015 PLT Design Inc.
 
-Racket is distributed under the GNU Lesser General Public License
-(LGPL).  This means that you can link Racket into proprietary
-applications, provided you follow the rules stated in the LGPL.  You can
-also modify Racket; if you distribute a modified version, you must
-distribute it under the terms of the LGPL, which in particular means
-that you must release the source code for the modified software.  See
-share/COPYING_LESSER.txt for more information.
+This file is distributed under the GNU Lesser General Public License
+(LGPL). This means that you can link this software into proprietary
+applications, provided you follow the rules stated in the LGPL. You
+can also modify this software; if you distribute a modified version,
+you must distribute it under the terms of the LGPL, which in
+particular means that you must release the source code for the
+modified software. See http://www.gnu.org/copyleft/lesser.html for
+more information.
 
 |#
--- scribblings/lexer-listings.scrbl
+++ scribblings/lexer-listings.scrbl
@@ -82,3 +82,14 @@ The @tt{RktValLink} choice may cause confusion, however, since symbols so styled
 @section{Acknowledgements}
 
 This document and parts of the software are based on Leif Andersen's work on the @racketmodfont{r-lexer} package. The rest of the software is based on code from Racket's @racketmodfont{scribble-lib} package.
+
+@; Copyright (c) 2015 leif, Tero
+@; 
+@; This file is distributed under the GNU Lesser General Public
+@; License (LGPL). This means that you can link this software into
+@; proprietary applications, provided you follow the rules stated in the
+@; LGPL. You can also modify this software; if you distribute a modified
+@; version, you must distribute it under the terms of the LGPL, which in
+@; particular means that you must release the source code for the
+@; modified software. See http://www.gnu.org/copyleft/lesser.html for
+@; more information.
